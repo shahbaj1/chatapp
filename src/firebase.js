12f912537c25655dccd4466063824a7c88c69ecf
shahbaj1 <shahbaja65@gmail.com>
2023-04-16 02:30:49 +0530

    changed-firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,15 @@
+
 import { initializeApp } from "firebase/app";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB6-Wby7K_iGyUt2eWFBHdX215uWRx33rs",
-  authDomain: "taj-chatapp.firebaseapp.com",
-  projectId: "taj-chatapp",
-  storageBucket: "taj-chatapp.appspot.com",
-  messagingSenderId: "57336175586",
-  appId: "1:57336175586:web:95bee81c09c375359c37bb"
+  apiKey: "AIzaSyD3xZMR762S8kPb5APco_f8zS7aEvVdC3E",
+  authDomain: "chatapp-9ccf8.firebaseapp.com",
+  projectId: "chatapp-9ccf8",
+  storageBucket: "chatapp-9ccf8.appspot.com",
+  messagingSenderId: "754840615431",
+  appId: "1:754840615431:web:b201f5e7be2c6647b088a2",
+  measurementId: "G-9B7ZSVK6NQ"
 };
 
 export const app = initializeApp(firebaseConfig);
